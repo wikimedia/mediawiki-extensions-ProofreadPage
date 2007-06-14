1b2ea770194186cc53fae7476e79f467b2a9a782
Raimond Spekking <raymond@users.mediawiki.org>
2007-06-14 15:42:53 +0000

    * (bug 10256) Update Russian translations   Patch by VasilievVV * Update German

--- ProofreadPage.i18n.php
+++ ProofreadPage.i18n.php
@@ -5,6 +5,14 @@ $messages = array(
 		'proofreadpage_namespace' => 'Page',
 		'proofreadpage_index_namespace' => 'Index',
 	),
+	'de' => array(
+		'proofreadpage_namespace' => 'Seite',
+		'proofreadpage_index_namespace' => 'Index',
+	),
+	'ru' => array(
+		'proofreadpage_namespace' => 'Страница',
+		'proofreadpage_index_namespace' => 'Индекс',
+	),
 );
 
 ?>
