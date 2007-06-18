d1283113050a23d67968cdd5b6638a23c307ee67
Raimond Spekking <raymond@users.mediawiki.org>
2007-06-18 08:07:59 +0000

    * (bug 10278) Update Georgian+Portuguese translations   Patch by Malafaya

--- ProofreadPage.i18n.php
+++ ProofreadPage.i18n.php
@@ -9,6 +9,14 @@ $messages = array(
 		'proofreadpage_namespace' => 'Seite',
 		'proofreadpage_index_namespace' => 'Index',
 	),
+	'ka' => array(
+		'proofreadpage_namespace' => 'გვერდი',
+		'proofreadpage_index_namespace' => 'ინდექსი',
+	),
+	'pt' => array(
+		'proofreadpage_namespace' => 'Página',
+		'proofreadpage_index_namespace' => 'Índice',
+	),
 	'ru' => array(
 		'proofreadpage_namespace' => 'Страница',
 		'proofreadpage_index_namespace' => 'Индекс',
