008db1b71dda514b60d469d05f4c4e8addf4831b
Jack Phoenix <ashley@users.mediawiki.org>
2010-09-27 14:43:15 +0000

    ProofreadPage: coding style tweaks to JS files

--- proofread_article.js
+++ proofread_article.js
@@ -1,21 +1,25 @@
 // Author : ThomasV - License : GPL
 
-/* add backlink to index page*/
+/* add backlink to index page */
 function pr_add_source() {
-	var a = document.getElementById("ca-nstab-main"); 
-	if(!a) return;
-	var q = document.getElementById("pr_index")
+	var a = document.getElementById( 'ca-nstab-main' );
+	if( !a ) {
+		return;
+	}
+	var q = document.getElementById( 'pr_index' );
 	if( q ) {
-		href = q.firstChild; 
-		if(!href) return;
-		q.removeChild(href);
+		href = q.firstChild;
+		if( !href ) {
+			return;
+		}
+		q.removeChild( href );
 		href.innerHTML = prp_source;
-		href.setAttribute("title", prp_source_message);
-		var new_li = document.createElement("li");
-		new_li.appendChild(href); 
-		var new_span = document.createElement("span");
-		new_span.appendChild(new_li); 
-		a.parentNode.insertBefore(new_span,a.nextSibling);
+		href.setAttribute( 'title', prp_source_message );
+		var new_li = document.createElement( 'li' );
+		new_li.appendChild( href );
+		var new_span = document.createElement( 'span' );
+		new_span.appendChild( new_li );
+		a.parentNode.insertBefore( new_span, a.nextSibling );
 	}
 }
-addOnloadHook(pr_add_source);
+addOnloadHook( pr_add_source );
