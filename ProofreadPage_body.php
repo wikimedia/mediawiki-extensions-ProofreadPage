41c503ffbe2ce45d397dfd3ec88fcee4cb208cf0
ThomasV <thomasv@users.mediawiki.org>
2010-09-28 09:39:04 +0000

    follow up to r73453

--- ProofreadPage_body.php
+++ ProofreadPage_body.php
@@ -840,17 +840,16 @@ var prp_default_footer = \"" . Xml::escapeJsString( wfMsgGetKey( 'proofreadpage_
 				list( $page, $pagenum ) = $item;
 				$pp[] = $page;
 			}
-			$pagelist = "'" . implode( "', '", $pp ) . "'";
 			$page_ns_index = MWNamespace::getCanonicalIndex( strtolower( $page_namespace ) );
 			$dbr = wfGetDB( DB_SLAVE );
-			$encCat = $dbr->addQuotes( str_replace( ' ' , '_' , wfMsgForContent( 'proofreadpage_quality0_category' ) ) );
+			$encCat = $dbr->strencode( str_replace( ' ' , '_' , wfMsgForContent( 'proofreadpage_quality0_category' ) ) );
 			$res = $dbr->select(
 				array( 'page', 'categorylinks' ),
 				array( 'page_title' ),
 				array(
-					"page_title IN ( $pagelist )",
-					"cl_to=$encCat",
-					"page_namespace=$page_ns_index"
+					'page_title' => $pp,
+					'cl_to' => $encCat,
+					'page_namespace' => $page_ns_index
 				),
 				__METHOD__,
 				null,
