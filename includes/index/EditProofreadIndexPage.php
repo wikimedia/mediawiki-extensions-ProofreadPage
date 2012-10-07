e90c05d653704d2765639d2e2570d3c1523e0f62
Tpt <thomaspt@hotmail.fr>
2012-10-07 20:57:10 +0200

    (bug 37419) Rewriting of Index pages edit form in PHP
    Rewriting of the index form in PHP in order to improve performance (the edit toolbars were loaded and showed and then Prp created the form) and allow easy editing of index pages
    Creation of json configuration message proofreadpage_index_data_config that replace proofreadpage_index_attributes and proofreadpage_js_attributes.
    Wiki can keep using the two old messages. The description of the syntax is done in ProofreadIndexPage::getDataConfig()
    This creation allow to add new features in the form:
    - list of possible values for a field (show as a select html element)
    - help messages
    - type for each data
    - default values
    There is no validation in this form in order to support existant data.
    The names and id of field has change: a prefix "wpprpindex-" is added in order to avoid any id conflict
    
    Some generic classes where added:
    - ProofreadIndexPage that offer methods in order to get index page content.
    - ProofreadIndexEntry that manage an entry in the index page and is configuration.
    Use of these two classes inside of existing parts of the ProofreeadPage extension will be done in an other commit.
    
    This change is the same as https://gerrit.wikimedia.org/r/#/c/10741/ but have been duplicate because of a bad manipulation.
    
    Patch set 3: Fix a bug on page creation.
    Patch set 4: Rebase + replace of wfMsg*
    
    Change-Id: Idc00bddac8abc58e0b6414bdd9dc3f32cab3337a

---
+++ includes/index/EditProofreadIndexPage.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup ProofreadPage
+ */
+
+class EditProofreadIndexPage extends EditPage {
+
+	/*
+	 * Store content of the from (useful for preview)
+	 */
+	protected $indexData;
+
+	protected function isSectionEditSupported() {
+		return false; // sections and forms don't mix
+	}
+
+	/**
+	 * Add custom fields
+	 */
+	protected function showContentForm() {
+		global $wgOut;
+
+		$pageLang = $this->mTitle->getPageLanguage();
+		$inputAttributes = array( 'lang' => $pageLang->getCode(), 'dir' => $pageLang->getDir() );
+		if( wfReadOnly() === true ) {
+			$inputAttributes['readonly'] = '';
+		}
+
+		if ( $this->indexData === null ) {
+			$index = new ProofreadIndexPage( $this->mTitle );
+			$entries = $index->getIndexEntries();
+		} else {
+			$entries = ProofreadIndexPage::getIndexEntriesFromIndexContent( $this->indexData );
+		}
+
+		$wgOut->addHTML( Xml::openElement( 'table', array( 'id' => 'prp-formTable' ) ) );
+
+		$i = 10;
+		foreach( $entries as $entry ) {
+			$inputAttributes['tabindex'] = $i;
+			$this->addEntry( $entry, $inputAttributes );
+			$i++;
+		}
+
+		$wgOut->addHTML( Xml::closeElement( 'table' ) );
+	}
+
+	/**
+	 * Add an entry to the form
+	 *
+	 * @param $entry ProofreadIndexEntry
+	 */
+	protected function addEntry( ProofreadIndexEntry $entry, $inputAttributes = array() ) {
+		global $wgOut;
+
+		if ( $entry->isHidden() ) {
+			return;
+		}
+		$key = $this->getFieldNameForEntry( $entry->getKey() );
+		$val = $this->safeUnicodeOutput( $entry->getStringValue() );
+
+		$wgOut->addHTML(
+			Xml::openElement( 'tr' ) .
+				Xml::openElement( 'th', array( 'scope' => 'row' ) ) .
+					Xml::label( $entry->getLabel(), $key )
+		);
+
+
+		$help = $entry->getHelp();
+		if ( $help !== '' ) {
+			$wgOut->addHTML(
+				Xml::openElement( 'span', array( 'class' => 'mw-help-field-container' ) ) .
+						Xml::element( 'span',  array( 'class' => 'mw-help-field-hint' ), ' ' ) .
+						Xml::element( 'span',  array( 'class' => 'mw-help-field-data' ), $help ) .
+				Xml::closeElement( 'span' )
+			);
+		}
+
+		$wgOut->addHTML(
+			Xml::closeElement( 'th' ) .
+			Xml::openElement( 'td' )
+		);
+
+		$values = $entry->getPossibleValues();
+		if ( $values !== null ) {
+			$select = new XmlSelect( $key, $key, $val );
+			foreach( $values as $value => $label ) {
+				$select->addOption( $label, $value );
+			}
+			if ( !isset( $values[$val] ) && $val !== '' ) { //compatiblity with already set data that aren't in the list
+				$select->addOption( $val, $val );
+			}
+			$wgOut->addHTML( $select->getHtml() );
+		} else {
+			$type = $entry->getType();
+			$inputType = ( $type === 'number' ) ? 'number' : 'text';
+			$size = $entry->getSize();
+			$inputAttributes['class'] = 'prp-input-' . $type;
+
+			if ( $size === 1 ) {
+				$inputAttributes['type'] = $inputType;
+				$inputAttributes['id'] = $key;
+				$wgOut->addHTML( Xml::input( $key, 60, $val, $inputAttributes ) );
+			} else {
+				$wgOut->addHTML( Xml::textarea( $key, $val, 60, $size, $inputAttributes ) );
+			}
+		}
+
+		$wgOut->addHTML(
+				Xml::closeElement( 'td' ) .
+			Xml::closeElement( 'tr' )
+		);
+	}
+
+	/**
+	 * Return the name of the edit field for an entry
+	 *
+	 * @param $key string the entry key
+	 */
+	protected function getFieldNameForEntry( $key ) {
+		return 'wpprpindex-' . $key;
+	}
+
+	/**
+	 * Init $this->textbox1 and $this->indexData from form content
+	 *
+	 * @param $request WebRequest
+	 */
+	protected function importContentFormData( &$request ) {
+		$config = ProofreadIndexPage::getDataConfig();
+		$this->textbox1 = "{{:MediaWiki:Proofreadpage_index_template";
+		$this->indexData = array();
+		foreach( $config as $key => $params ) {
+			$field = $this->getFieldNameForEntry( $key );
+			$value = $this->cleanInputtedContent( $request->getVal( $field ) );
+			$this->indexData[$key] = $value;
+			$this->textbox1 .= "\n|" . $key . "=" . $value;
+		}
+		$this->textbox1 .= "\n}}";
+	}
+
+	/**
+	 * Clean a text before inclusion into a template
+	 *
+	 * @param $value string
+	 * @return string
+	 */
+	function cleanInputtedContent( $value ) {
+		// remove trailing \n \t or \r
+		$value = trim( $value );
+
+		// replace pipe symbol everywhere...
+		$value = preg_replace( '#\|#', '&!&', $value );
+
+		// ...except in links...
+		$prev = '';
+		do {
+			$prev = $value;
+			$value = preg_replace( '#\[\[(.*?)&!&(.*?)\]\]#', '[[$1|$2]]', $value );
+		} while ( $value != $prev );
+
+		// ..and in templates
+		do {
+			$prev = $value;
+			$value = preg_replace( '#\{\{(.*?)&!&(.*?)\}\}#', '{{$1|$2}}', $value );
+		} while ( $value != $prev );
+
+		$value = preg_replace( '#&!&#', '{{!}}', $value );
+
+		return $value;
+	}
+}
--- modules/ext.proofreadpage.index/ext.proofreadpage.index.js
+++ modules/ext.proofreadpage.index/ext.proofreadpage.index.js
@@ -2,137 +2,39 @@
 
 // the index template is a system message.
 // another message in i18n lists the parameters
-function findparam( str2, param_name ) {
-	var pattern = "\\n\\|" + param_name + "=([\\s\\S]*?)\\n\\|([^=\\n\\|]+)=";
-	var re2 = new RegExp( pattern );
-	var m2 = str2.match( re2 );
-	if( m2 ) {
-		return m2[1];
-	}
-	return '';
-}
 
-function proofreadpage_index_init() {
-	var toolbar = document.getElementById( 'toolbar' );
-	if( toolbar ) {
-		toolbar.parentNode.removeChild( toolbar );
-	}
+jQuery( function( $ ) {
 
-	var text = document.getElementById( 'wpTextbox1' );
-	if( !text ) {
-		return;
-	}
-
-	params = '';
-	if( text.value ) {
-		var re = /\{\{:MediaWiki:Proofreadpage_index_template([\s\S]*)\n\}\}/m;
-		var m = text.value.match( re );
-		if( !m ) {
-			return;
+	$( 'input.prp-input-page' ).autocomplete( {
+		source: function( request, response ) {
+			$.getJSON(
+				mw.util.wikiScript( 'api' ), {
+					'action': 'opensearch',
+					'search': request.term
+				}, function ( data ) {
+					if ( $.isArray( data ) && 1 in data ) {
+						response( $.map( data[1], function( item ) {
+							return {
+								label: item,
+								value: item
+							};
+						}));
+					}
+				}
+			);
+		},
+		minLength: 2,
+		select: function(event, ui) {
+			this.value = ui.item.value;
 		}
-		params = m[1] + '\n\|END=';
-	}
+	});
 
-	// Certain extensions, like UsabilityInitiative, wrap the textarea
-	var textWrapper = text;
-	while( textWrapper.parentNode.nodeName != 'FORM' ) {
-		textWrapper = textWrapper.parentNode;
-	}
-	var f = textWrapper.parentNode;
-	var container = document.createElement( 'div' );
-	var wrapper = document.createElement( 'div' );
-	wrapper.style.display = 'none';
-	container.appendChild( wrapper );
-	wrapper.appendChild( textWrapper );
-
-	var index_attributes = self.prp_index_attributes.split( '\n' );
-	var str = '<table>';
-	for( i = 0; i < index_attributes.length; i++ ) {
-		m = index_attributes[i].split( '|' );
-		param_name = m[0];
-
-		if( m[1] ) {
-			param_label = m[1];
-		} else {
-			param_label = param_name;
-		}
-		str += '<tr><td>' + param_label + ': </td>';
-
-		value = findparam( params, param_name );
-		value = value.replace( /\{\{!\}\}/g, '|' );
-
-		if( ( m[0] == 'Header' ) && ( value == '' ) ) {
-			value = prp_default_header;
-		}
-		if( ( m[0] == 'Footer' ) && ( value == '' ) ) {
-			value = prp_default_footer;
-		}
-
-		if( m[2] ) {
-			size = m[2];
-		} else {
-			size = '1';
-		}
-		if( size == '1' ) {
-			str += '<td><input name="' + param_name + '" size=60 value="' + value + '"/></td></tr>';
-		} else {
-			str += '<td><textarea name="' + param_name + '" cols=60 rows=' + size + '>' + value + '</textarea></td></tr>';
-		}
-	}
-	str += '</table>';
-	container.innerHTML += str;
-
-	var saveButton = document.getElementById( 'wpSave' );
-	var previewButton = document.getElementById( 'wpPreview' );
-	var diffButton = document.getElementById( 'wpDiff' );
-	if( saveButton ) {
-		saveButton.onclick = proofreadpage_fill_index;
-		previewButton.onclick = proofreadpage_fill_index;
-		diffButton.onclick = proofreadpage_fill_index;
-	} else {
-		container.firstChild.nextSibling.setAttribute( 'readonly', 'readonly' );
-	}
-
-	copywarn = document.getElementById( 'editpage-copywarn' );
-	if( copywarn.parentNode.nodeName == 'FORM' ) {
-		f.insertBefore( container, copywarn );
-	} else {
-		f.insertBefore( container, copywarn.parentNode );
-	}
-}
-
-function proofreadpage_fill_index() {
-	var form = document.getElementById( 'editform' );
-	var result = '{{:MediaWiki:Proofreadpage_index_template';
-	var index_attributes = self.prp_index_attributes.split( '\n' );
-
-	for( i = 0; i < index_attributes.length; i++ ) {
-		m = index_attributes[i].split( '|' );
-		param_name = m[0];
-
-		value = form.elements[param_name].value;
-		// remove trailing \n
-		value = value.replace( /\n$/, '' );
-
-		// replace pipe symbol everywhere...
-		value = value.replace( /\|/g, '{{!}}' );
-
-		// ...except in links...
-		do {
-			prev = value;
-			value = value.replace( /\[\[(.*?)\{\{!\}\}(.*?)\]\]/g, '[[$1|$2]]' );
-		} while ( value != prev );
-
-		// ..and in templates
-		do {
-			prev = value;
-			value = value.replace( /\{\{(.*?)\{\{!\}\}(.*?)\}\}/g, '{{$1|$2}}' );
-		} while ( value != prev );
-
-		result = result + '\n|' + param_name + '=' + value;
-	}
-	result = result + "\n}}";
-	form.elements['wpTextbox1'].value = result;
-}
-
-jQuery( proofreadpage_index_init );
+	// Set up the help system
+	$( '.mw-help-field-data' )
+		.hide()
+		.closest( '.mw-help-field-container' ).find( '.mw-help-field-hint' )
+			.show()
+			.click( function() {
+				$(this).closest( '.mw-help-field-container' ).find( '.mw-help-field-data' ).slideToggle( 'fast' );
+			} );
+});
