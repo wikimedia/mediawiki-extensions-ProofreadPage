e0fefb116eb76203dcd81d36a86a2ab5579771aa
Tpt <thomaspt@hotmail.fr>
2012-11-23 21:46:36 +0100

    Remove the autocomplete for fields of type "page"
    Change-Id: I1025b6935c0b9cf935c6b6946163dd93b8949df6

--- modules/ext.proofreadpage.index/ext.proofreadpage.index.js
+++ modules/ext.proofreadpage.index/ext.proofreadpage.index.js
@@ -5,30 +5,6 @@
 
 jQuery( function( $ ) {
 
-	$( 'input.prp-input-page' ).autocomplete( {
-		source: function( request, response ) {
-			$.getJSON(
-				mw.util.wikiScript( 'api' ), {
-					'action': 'opensearch',
-					'search': request.term
-				}, function ( data ) {
-					if ( $.isArray( data ) && 1 in data ) {
-						response( $.map( data[1], function( item ) {
-							return {
-								label: item,
-								value: item
-							};
-						}));
-					}
-				}
-			);
-		},
-		minLength: 2,
-		select: function(event, ui) {
-			this.value = ui.item.value;
-		}
-	});
-
 	// Set up the help system
 	$( '.mw-help-field-data' )
 		.hide()
