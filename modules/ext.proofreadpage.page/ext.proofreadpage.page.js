54dd7a6ca889d1311f21ff7ec9d8674333ec0888
Aarti Dwivedi <ellydwivedi2093@gmail.com>
2013-09-20 17:07:14 +0530

    Added zoom buttons to the toolbar.
    Change-Id: I26d017b1f4556efb83eeb1aa924e1b7e72118dfe

--- modules/ext.proofreadpage.page/ext.proofreadpage.page.js
+++ modules/ext.proofreadpage.page/ext.proofreadpage.page.js
@@ -26,10 +26,137 @@
 	} );
 } ( jQuery, mediaWiki ) );
 
+function initPanZoom() {
+	$('.prp-page-image img').panZoom({
+		'debug'	: false
+	});
+}
+
 function prStartup() {
 	jQuery( function() {
-		prInitTabs();
-	} );
+		prAddButtons();
+		initPanZoom();
+	});
+}
+
+function prAddButtons() {
+
+	if( !proofreadPageIsEdit ) {
+		return;
+	}
+	var tools = {
+		'section': 'proofreadpage-tools',
+		'groups': {
+			'zoom': {
+				'label': mw.msg( 'proofreadpage-group-zoom' ),
+				'tools': {
+					'zoom-in': {
+						label: mw.msg( 'proofreadpage-button-zoom-in-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_zoom_in.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('zoomIn');
+							}
+						}
+					},
+					'zoom-out': {
+						label: mw.msg( 'proofreadpage-button-zoom-out-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_zoom_out.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('zoomOut');
+							}
+						}
+					},
+					'reset-zoom': {
+						label: mw.msg( 'proofreadpage-button-reset-zoom-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_examine.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('fit');
+							}
+						}
+					},
+					'pan-up': {
+						label: mw.msg( 'proofreadpage-button-pan-up-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_pan_up.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('panUp');
+							}
+						}
+					},
+					'pan-down': {
+						label: mw.msg( 'proofreadpage-button-pan-down-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_pan_down.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('panDown');
+							}
+						}
+					},
+					'pan-left': {
+						label: mw.msg( 'proofreadpage-button-pan-left-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_pan_left.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('panLeft');
+							}
+						}
+					},
+					'pan-right': {
+						label: mw.msg( 'proofreadpage-button-pan-right-label' ),
+						type: 'button',
+						icon: mw.config.get( 'wgExtensionAssetsPath' ) + '/ProofreadPage/modules/ext.proofreadpage.page/images/Button_pan_right.png',
+						action: {
+							type: 'callback',
+							execute: function() {
+								$( '.prp-page-image img' ).panZoom('panRight');
+							}
+						}
+					}
+				}
+			},
+		}
+	};
+
+	var $edit = $( '#wpTextbox1' );
+	if( mw.user.options.get('usebetatoolbar') ) {
+		mw.loader.using( 'ext.wikiEditor.toolbar', function() {
+			$edit.wikiEditor( 'addToToolbar', {
+				'sections': {
+					'proofreadpage-tools': {
+						'type': 'toolbar',
+						'label': mw.msg( 'proofreadpage-section-tools' )
+					}
+				}
+			} )
+			.wikiEditor( 'addToToolbar', tools);
+		});
+	} else {
+		$.each( tools.groups, function( group, list ) {
+			$.each( list.tools, function( id, def ) {
+				mw.toolbar.addButton( {
+					imageFile: def.icon,
+					imageId: 'mw-editbutton-' + id,
+					speedTip: def.label
+				} );
+			$( '#mw-editbutton-' + id ).click( def.action.execute );
+			} );
+		} );
+	}
 }
 
 if ( mw.user.options.get( 'usebetatoolbar' ) && jQuery.inArray( 'ext.wikiEditor.toolbar', mw.loader.getModuleNames() ) > -1 ) {
