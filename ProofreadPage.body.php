454e34792cdd184d764df352407135141956c285
Tpt <thomaspt@hotmail.fr>
2013-11-29 20:08:29 +0100

    Should fix an issue where qualityX classes are not add to links
    Change-Id: Ic7b371a9e76d9cc90f04abe32d46838372d35ec2

--- ProofreadPage.body.php
+++ ProofreadPage.body.php
@@ -212,8 +212,8 @@ class ProofreadPage {
 	 * @return bool
 	 */
 	public static function onGetLinkColours( $page_ids, &$colours ) {
-		global $wgTitle;
-		if ( !isset( $wgTitle ) ) {
+		global $wgParser;
+		if ( !isset( $wgParser ) ) {
 			return true;
 		}
 		self::getLinkColours( $page_ids, $colours );
@@ -226,10 +226,10 @@ class ProofreadPage {
 	 * @param $colours array
 	 */
 	private static function getLinkColours( $page_ids, &$colours ) {
-		global $wgTitle;
+		global $wgParser;
 
 		$page_namespace_id = self::getPageNamespaceId();
-		$in_index_namespace = $wgTitle->inNamespace( self::getIndexNamespaceId() );
+		$in_index_namespace = $wgParser->getTitle()->inNamespace( self::getIndexNamespaceId() );
 
 		$values = array();
 		foreach ( $page_ids as $id => $pdbk ) {
