a89889696cccb9c0bf1e3472f1de54a6023d046e
paladox <thomasmulhall410@yahoo.com>
2015-08-11 10:12:10 +0100

    Remove i18n shim
    Change-Id: Ia3b29a4ae9969b6810579c7ca0713e998ef53bc0

--- ProofreadPage.php
+++ ProofreadPage.php
@@ -40,7 +40,6 @@ define( 'CONTENT_MODEL_PROOFREAD_PAGE', 'proofread-page' );
 
 $dir = __DIR__ . '/';
 $wgMessagesDirs['ProofreadPage'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['ProofreadPage'] = $dir . 'ProofreadPage.i18n.php';
 $wgExtensionMessagesFiles['ProofreadPageAlias'] = $dir . 'ProofreadPage.alias.php';
 
 $wgAutoloadClasses['ProofreadPage'] = $dir . 'ProofreadPage.body.php';
