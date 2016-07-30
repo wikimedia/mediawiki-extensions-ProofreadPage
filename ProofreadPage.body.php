f9fcd8419c5e7560ce4fb0831f096ea67701f4c8
aufcl <AuFCL@challis.id.au>
2016-07-31 09:31:47 +1000

    Format proofreadpage-indexquality-alt using raw page counts not percentages
    Restore prior semantics matching intention of current language strings, as well as removing ludicrous-precision floating point value reporting.
    
    Bug: T119740
    Change-Id: I77c7f20958708f1a13e53ae56e4b4c9bf8abee8b

--- ProofreadPage.body.php
+++ ProofreadPage.body.php
@@ -569,7 +569,7 @@ class ProofreadPage {
 		$q4 = $n4 * 100 / $n;
 		$qe = $ne * 100 / $n;
 		$void_cell = $ne ? '<td class="qualitye" style="width:' . $qe . '%;"></td>' : '';
-		$textualAlternative = wfMessage( 'proofreadpage-indexquality-alt', $q4, $q3, $q1 );
+		$textualAlternative = wfMessage( 'proofreadpage-indexquality-alt', $n4, $n3, $n1 );
 		$output = '<table class="pr_quality" title="' . $textualAlternative . '">
 <tr>
 <td class="quality4" style="width:' . $q4 . '%;"></td>
