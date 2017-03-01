4106f8b98a19f5d5ea6a867d3ca92efeb56cddda
Ed Sanders <esanders@wikimedia.org>
2017-03-01 12:58:40 +0000

    VE: Don't load PRP target on all pages, just PRP pages.
    Change-Id: Id6b07806f6fc6499198c9c3aea96ba40cf3e0c70

---
+++ modules/ve/pageTarget/ve.init.mw.ProofreadPagePageTarget.init.js
@@ -0,0 +1,15 @@
+/*!
+ * VisualEditor ProofreadPagePageTarget initialization.
+ *
+ * @copyright 2011-2016 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+( function () {
+	mw.loader.using( 'ext.visualEditor.desktopArticleTarget.init', function () {
+		// Adding this module to VisualEditorPluginModules would cause it to load on
+		// all VE-loading pages (even non PRP namespaces). Instead add it to the plugin
+		// list here, only on pages that have loaded 'ext.proofreadpage.page' first.
+		mw.libs.ve.addPlugin( 'ext.proofreadpage.ve.pageTarget' );
+	} );
+} )();
