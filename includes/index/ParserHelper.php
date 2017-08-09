b630e563229a41d44c1652711728221ebb052cc5
Tpt <thomaspt@hotmail.fr>
2017-08-09 14:35:10 -0400

    Renders Index: pages without relying on their Wikitext serialization
    Change-Id: I3f3ffcae202a08acec55061dc07a20f033fa8d69

---
+++ includes/index/ParserHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ProofreadPage\Index;
+
+use Parser;
+use ParserOptions;
+use Title;
+
+/**
+ * @licence GNU GPL v2+
+ *
+ * Content of a Index: page
+ */
+class ParserHelper {
+
+	/**
+	 * @var Parser
+	 */
+	private $parser;
+
+	public function __construct( Title $title = null, ParserOptions $options ) {
+		$this->parser = new Parser();
+		$this->parser->startExternalParse( $title, $options, Parser::OT_PLAIN );
+	}
+
+	/**
+	 * @param string $text the wikitext that should see its template arguments expanded
+	 * @param string[] $args the arguments to use during the expansion
+	 * @return string
+	 */
+	public function expandTemplateArgs( $text, array $args ) {
+		// We build a frame with the arguments for the template
+		$frame = $this->parser->getPreprocessor()->newCustomFrame( $args );
+
+		// We replace the arguments calls by their values
+		$dom = $this->parser->preprocessToDom( $text, Parser::PTD_FOR_INCLUSION );
+		$text = $frame->expand( $dom );
+
+		// We take care of removing the tags placeholders
+		return $this->parser->mStripState->unstripBoth( $text );
+	}
+
+	public function fetchTemplateTextAndTitle( Title $title ) {
+		return $this->parser->fetchTemplateAndTitle( $title );
+	}
+}
