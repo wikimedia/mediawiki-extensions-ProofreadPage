2a8f6d40bc81f2c46525a75b8aba0a1a741c4292
Tpt <thomaspt@hotmail.fr>
2018-06-30 12:01:52 +0200

    Fallbacks to the quality category when the page quality level page property is not set yet
    Bug: T198470
    Change-Id: Id59f49b435281d0c08a96006e6aaf08eb8066f6b

--- includes/Page/DatabasePageQualityLevelLookup.php
+++ includes/Page/DatabasePageQualityLevelLookup.php
@@ -9,10 +9,20 @@ use Title;
  * @license GPL-2.0-or-later
  *
  * Allows to retrieve the quality level of a Page: page
+ *
+ * TODO: drop the category fallback when all Wikisource pages will have the relevant page property
  */
 class DatabasePageQualityLevelLookup implements PageQualityLevelLookup {
 
+	/**
+	 * @var int
+	 */
 	private $pageNamespaceId;
+
+	/**
+	 * @var Title[]
+	 */
+	private $categoryForQualityLevel;
 
 	private $cache = [];
 
@@ -44,10 +54,6 @@ class DatabasePageQualityLevelLookup implements PageQualityLevelLookup {
 				!array_key_exists( $pageTitle->getDBkey(), $this->cache );
 		} );
 
-		if ( empty( $pageTitles ) ) {
-			return;
-		}
-
 		$this->fetchQualityLevelForPageTitles( $pageTitles );
 	}
 
@@ -58,6 +64,20 @@ class DatabasePageQualityLevelLookup implements PageQualityLevelLookup {
 		// We set to unknown all qualities
 		foreach ( $pageTitles as $pageTitle ) {
 			$this->cache[$pageTitle->getDBkey()] = null;
+		}
+
+		$this->fetchQualityLevelForPageTitlesFromPageProperties( $pageTitles );
+		$this->fetchQualityLevelForPageTitlesFromPageCategories(
+			$this->filterPagesWithoutKnownQuality( $pageTitles )
+		);
+	}
+
+	/**
+	 * @param Title[] $pageTitles
+	 */
+	private function fetchQualityLevelForPageTitlesFromPageProperties( array $pageTitles ) {
+		if ( empty( $pageTitles ) ) {
+			return;
 		}
 
 		$results = wfGetDB( DB_REPLICA )->select(
@@ -76,5 +96,63 @@ class DatabasePageQualityLevelLookup implements PageQualityLevelLookup {
 		foreach ( $results as $result ) {
 			$this->cache[$result->page_title] = $result->pp_value;
 		}
+	}
+
+	/**
+	 * @param Title[] $pageTitles
+	 */
+	private function fetchQualityLevelForPageTitlesFromPageCategories( array $pageTitles ) {
+		if ( empty( $pageTitles ) ) {
+			return;
+		}
+
+		$pageDbKeys = array_map( function ( Title $title ){
+			return $title->getDBkey();
+		}, $pageTitles );
+
+		foreach ( $this->getCategoryForQualityLevels() as $qualityLevel => $qualityCategory ) {
+			$results = wfGetDB( DB_REPLICA )->select(
+				[ 'categorylinks', 'page' ],
+				[ 'page_title' ],
+				[
+					'page_id = cl_from',
+					'page_title' => $pageDbKeys,
+					'cl_to' => $qualityCategory->getDBkey(),
+					'page_namespace' => $this->pageNamespaceId
+				],
+				__METHOD__
+			);
+
+			foreach ( $results as $result ) {
+				$this->cache[$result->page_title] = $qualityLevel;
+			}
+		}
+	}
+
+	private function filterPagesWithoutKnownQuality( array $pageTitles ) {
+		return array_filter( $pageTitles, function ( Title $pageTitle ) {
+			return $this->cache[$pageTitle->getDBkey()] === null;
+		} );
+	}
+
+	private function getCategoryForQualityLevels() {
+		if ( $this->categoryForQualityLevel === null ) {
+			$this->categoryForQualityLevel = $this->computeCategoryForQualityLevels();
+		}
+		return $this->categoryForQualityLevel;
+	}
+
+	private function computeCategoryForQualityLevels() {
+		$qualityCategories = [];
+		for ( $qualityLevel = 0; $qualityLevel <= 4; $qualityLevel++ ) {
+			$categoryTitle = Title::makeTitleSafe(
+				NS_CATEGORY,
+				wfMessage( "proofreadpage_quality{$qualityLevel}_category" )->inContentLanguage()->text()
+			);
+			if ( $categoryTitle !== null ) {
+				$qualityCategories[$qualityLevel] = $categoryTitle;
+			}
+		}
+		return $qualityCategories;
 	}
 }
