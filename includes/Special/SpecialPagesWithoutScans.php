cff508450c44edfee3cda21efc5e32744d8b0d7d
Tpt <thomaspt@hotmail.fr>
2018-09-22 11:34:01 +0200

    Refactors Special:PagesWithoutScan to make use of Extension:Disambiguator
    Reuses the shared links list rendering code for simplicity
    
    Change-Id: I14eaa6b26eb5cb90a49ef0d2cfbea60d434ea171

--- includes/Special/SpecialPagesWithoutScans.php
+++ includes/Special/SpecialPagesWithoutScans.php
@@ -1,36 +1,16 @@
 <?php
-/**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @file
- * @ingroup SpecialPage
- */
 
 namespace ProofreadPage\Special;
 
-use LinkBatch;
-use ProofreadPage;
-use QueryPage;
-use Title;
+use PageQueryPage;
+use ProofreadPage\Context;
 
 /**
+ * @license GPL-2.0-or-later
+ *
  * Special page that lists the texts that have no transclusions
- * Pages in MediaWiki:Proofreadpage_notnaked_category are excluded.
  */
-class SpecialPagesWithoutScans extends QueryPage {
+class SpecialPagesWithoutScans extends PageQueryPage {
 	public function __construct( $name = 'PagesWithoutScans' ) {
 		parent::__construct( $name );
 	}
@@ -43,121 +23,32 @@ class SpecialPagesWithoutScans extends QueryPage {
 		return false;
 	}
 
-	/**
-	 * Return a clause with the list of disambiguation templates.
-	 * This function was copied verbatim from specials/SpecialDisambiguations.php
-	 * @param \Wikimedia\Rdbms\IDatabase $dbr
-	 * @return mixed
-	 */
-	public function getDisambiguationTemplates( $dbr ) {
-		$dMsgText = wfMessage( 'proofreadpage-disambiguationspage' )->inContentLanguage()->text();
-
-		$linkBatch = new LinkBatch;
-
-		# If the text can be treated as a title, use it verbatim.
-		# Otherwise, pull the titles from the links table
-		$dp = Title::newFromText( $dMsgText );
-		if ( $dp ) {
-			if ( $dp->getNamespace() != NS_TEMPLATE ) {
-				# FIXME we assume the disambiguation message is a template but
-				# the page can potentially be from another namespace :/
-				wfDebug( "Mediawiki:proofreadpage-disambiguationspage message " .
-					"does not refer to a template!\n" );
-			}
-			$linkBatch->addObj( $dp );
-		} else {
-			# Get all the templates linked from the Mediawiki:Disambiguationspage
-			$disPageObj = Title::makeTitleSafe( NS_MEDIAWIKI, 'disambiguationspage' );
-			$res = $dbr->select(
-				[ 'pagelinks', 'page' ],
-				'pl_title',
-				[ 'page_id = pl_from', 'pl_namespace' => NS_TEMPLATE,
-					'page_namespace' => $disPageObj->getNamespace(),
-					'page_title' => $disPageObj->getDBkey() ],
-				__METHOD__ );
-
-			foreach ( $res as $row ) {
-				$linkBatch->addObj( Title::makeTitle( NS_TEMPLATE, $row->pl_title ) );
-			}
-		}
-		return $linkBatch->constructSet( 'tl', $dbr );
-	}
-
 	public function getQueryInfo() {
-		$dbr = wfGetDB( DB_REPLICA );
-
-		// Construct subqueries
-		$pagesWithScansSubquery = $dbr->selectSQLText(
-			[ 'templatelinks', 'page' ],
-			'DISTINCT tl_from',
-			[
-				'page_id=tl_from',
-				'tl_namespace' => ProofreadPage::getPageNamespaceId(),
-				'page_namespace' => NS_MAIN
-			]
-		);
-
-		// Exclude disambiguation pages too
-		// FIXME: Update to filter against 'disambiguation' page property
-		// instead. See https://www.mediawiki.org/wiki/Extension:Disambiguator.
-		// May want to verify that wikis using ProofreadPage have implemented
-		// the __DISAMBIG__ magic word for their disambiguation pages before
-		// changing this.
-		$dt = $this->getDisambiguationTemplates( $dbr );
-		$disambigPagesSubquery = $dbr->selectSQLText(
-			[ 'page', 'templatelinks' ],
-			'page_id',
-			[
-				'page_id=tl_from',
-				'page_namespace' => NS_MAIN,
-				$dt
-			]
-		);
-
+		$context = Context::getDefaultContext();
 		return [
-			'tables' => 'page',
+			'tables' => [ 'page', 'templatelinks', 'page_props' ],
 			'fields' => [
-				"'PagesWithoutScans' AS type",
 				'page_namespace AS namespace',
 				'page_title AS title',
-				'page_len AS value' ],
+				'page_len AS value'
+			],
 			'conds' => [
 				'page_namespace' => NS_MAIN,
 				'page_is_redirect' => 0,
-				"page_id NOT IN ($pagesWithScansSubquery)",
-				"page_id NOT IN ($disambigPagesSubquery)" ],
-			'options' => [ 'USE INDEX' => 'page_len' ]
+				'tl_from' => null,
+				'pp_page' => null
+			],
+			'join_conds' => [
+				'templatelinks' => [
+					'LEFT JOIN',
+					[ 'page_id = tl_from', 'tl_namespace' => $context->getPageNamespaceId() ]
+				],
+				'page_props' => [
+					'LEFT JOIN',
+					[ 'page_id = pp_page', 'pp_propname' => 'disambiguation' ]
+				]
+			],
 		];
-	}
-
-	public function sortDescending() {
-		return true;
-	}
-
-	public function formatResult( $skin, $result ) {
-		global $wgContLang;
-		$dm = $wgContLang->getDirMark();
-
-		$title = Title::makeTitleSafe( $result->namespace, $result->title );
-		if ( !$title ) {
-			return '<!-- Invalid title ' .
-				htmlspecialchars( "{$result->namespace}:{$result->title}" ) . '-->';
-		}
-		$linkRenderer = $this->getLinkRenderer();
-		$hlink = $linkRenderer->makeKnownLink(
-			$title,
-			$this->msg( 'hist' )->text(),
-			[],
-			[ 'action' => 'history' ]
-		);
-		$plink = $this->isCached()
-			? $linkRenderer->makeLink( $title )
-			: $linkRenderer->makeKnownLink( $title );
-		$size = $this->msg( 'nbytes', $result->value )->escaped();
-
-		return $title->exists()
-				? "({$hlink}) {$dm}{$plink} {$dm}[{$size}]"
-				: "<s>({$hlink}) {$dm}{$plink} {$dm}[{$size}]</s>";
 	}
 
 	protected function getGroupName() {
