07b257778d3b2c043d17babf6efca4312033afdc
Tpt <thomaspt@hotmail.fr>
2019-08-19 11:13:12 +0200

    Adds an editing widget for Index: pages categories editing
    Change-Id: Ieb16ad4a7a52d8ae2cf7794eea0ca47649a97042

--- modules/index/ext.proofreadpage.index.js
+++ modules/index/ext.proofreadpage.index.js
@@ -1,7 +1,21 @@
-mw.loader.using( 'oojs-ui-core' ).done( function () {
+mw.loader.using( 'mediawiki.widgets.CategoryMultiselectWidget' ).done( function () {
 	$( function () {
 		$( '.prp-fieldLayout-help' ).map( function () {
 			return OO.ui.infuse( this );
+		} );
+
+		// Categories
+		$( '#wpPrpCategories' ).map( function () {
+			var categoryInputWidget = OO.ui.infuse( this.parentElement ),
+				categorySelector = new mw.widgets.CategoryMultiselectWidget( {} );
+
+			categorySelector.setValue( categoryInputWidget.getValue().split( '|' ) );
+			categorySelector.on( 'change', function () {
+				categoryInputWidget.setValue( categorySelector.getValue().join( '|' ) );
+			} );
+
+			categoryInputWidget.$element.before( categorySelector.$element );
+			categoryInputWidget.toggle( false );
 		} );
 	} );
 } );
