7fdbb197c026ab68e1e99490879bc0852f2caa35
Tpt <thomaspt@hotmail.fr>
2019-08-21 22:04:56 +0200

    Do not take empty string as Category title in Index: pages editing UI
    Change-Id: I23e0f47cba1041f6da1b33a9aec522f08ccd4e66

--- modules/index/ext.proofreadpage.index.js
+++ modules/index/ext.proofreadpage.index.js
@@ -1,15 +1,17 @@
 mw.loader.using( 'mediawiki.widgets.CategoryMultiselectWidget' ).done( function () {
 	$( function () {
-		$( '.prp-fieldLayout-help' ).map( function () {
-			return OO.ui.infuse( this );
+		$( '.prp-fieldLayout-help' ).each( function () {
+			OO.ui.infuse( this );
 		} );
 
 		// Categories
-		$( '#wpPrpCategories' ).map( function () {
-			var categoryInputWidget = OO.ui.infuse( this.parentElement ),
+		$( '#wpPrpCategories' ).each( function () {
+			var categoryInputWidget = OO.ui.infuse( this.parentNode ),
 				categorySelector = new mw.widgets.CategoryMultiselectWidget( {} );
 
-			categorySelector.setValue( categoryInputWidget.getValue().split( '|' ) );
+			categorySelector.setValue( categoryInputWidget.getValue().split( '|' ).filter( function ( value ) {
+				return value !== '';
+			} ) );
 			categorySelector.on( 'change', function () {
 				categoryInputWidget.setValue( categorySelector.getValue().join( '|' ) );
 			} );
