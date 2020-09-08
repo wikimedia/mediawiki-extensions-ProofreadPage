wikimedia/mediawiki-extensions-ProofreadPage

commit 818c206b81d4ebd4cd45157a0c7ee9d13b4b9bd3

Fixed misplaced warning in visual mode
Fixed bug where the
proofreadpage-pagelist-dialog-visual-number-field-disabled-but-active
warning apeared despite the user not having entered a non-number
compatible label in the ComboboxInput

Bug: T253458
Change-Id: I57a4cd331a78a5f87609ac027187ffc7473f41e8

modules/index.pagelist/PagelistInputWidget.VisualFormPanel.js
@@ -77,10 +77,6 @@ function VisualFormPanel( VisualDialogModel, config ) {
 		changedToNumberingIncompatibleValue: 'disableNumberField'
 	} );
 
-	this.numberInput.connect( this, {
-		change: 'disableNumberField'
-	} );
-
 	this.updateButton.connect( this, {
 		click: 'onUpdate'
 	} );
@@ -143,6 +139,7 @@ VisualFormPanel.prototype.disableNumberField = function () {
 		this.numberInputLayout.setWarnings( [
 			mw.msg( 'proofreadpage-pagelist-dialog-visual-number-field-disabled-but-active' )
 		] );
+		this.numberInput.once( 'change', this.disableNumberField.bind( this ) );
 	}
 
 	this.enableUpdateButton();

commit 63d8344c4c0b950ea1305cd72f47a3d2c04dd107

Fix updateButton not getting activated on NumberInput change
Bug introduced by 818c206b81d4ebd4cd45157a0c7ee9d13b4b9bd3 where
the update button wasn't being enabled even after the user started
typing into the NumberInputWidget in visual mode.

Bug: T54321
Change-Id: I00a32aab17eac9dc5725d3d1e126d2f64c0fe1f4

modules/index.pagelist/PagelistInputWidget.VisualFormPanel.js
@@ -76,6 +76,9 @@ function VisualFormPanel( VisualDialogModel, config ) {
 		changedToNumberingCompatibleValue: 'enableNumberField',
 		changedToNumberingIncompatibleValue: 'disableNumberField'
 	} );
+	this.numberInput.connect( this, {
+		change: 'enableUpdateButton'
+	} );
 
 	this.updateButton.connect( this, {
 		click: 'onUpdate'