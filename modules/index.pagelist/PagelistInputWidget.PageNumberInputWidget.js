wikimedia/mediawiki-extensions-ProofreadPage

commit fc745f5dfb483766e12cf4123ef08cc1427d37a4

Use mw.config.get() to retrieve builtin labels for Pagelist Widget
Added a new mw.config variable 'proofreadpage_builtin_pagelist_labels'
that contains the labels/ranges that are provided by default by
ProofreadPage.

Changed PagelistInputWidget's label checking logic to use the new
config variable so as to prevent PagelistInputWidget from getting
out of sync with PageNumber.php.

Follow up of Ica93e67cd3558e8771d765382bd7a85b50c7601a.

Bug: T268906
Change-Id: Ic1ea47a2bdd17dae17178bbe94c5afc61f3c1fa2

modules/index.pagelist/PagelistInputWidget.PageNumberInputWidget.js
@@ -112,8 +112,11 @@ PageNumberInputWidget.prototype.getNumberTypeConfig = function () {
  */
 PageNumberInputWidget.prototype.updateFormPanel = function () {
 	var data = this.pageNumberTypeInput.getValue();
-	// @todo: Improve this implementation
-	if ( data === 'Number' || data === 'roman' || data === 'highroman' ) {
+
+	// Check if the label is one that is provided by ProofreadPage by default
+	// and changes the number format being displayed.
+	if ( mw.config.get( 'prpPagelistBuiltinLabels' ) &&
+	mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( data.label ) === -1 && data.label !== 'empty' ) {
 		if ( data === 'Number' ) {
 			this.emit( 'changedToSinglePageIncompatibleValue' );
 		} else {

modules/index.pagelist/PagelistInputWidget.VisualDialogModel.js
@@ -144,10 +144,12 @@ VisualDialogModel.prototype.setRangesForOverlappingRanges = function ( params, d
  * @return {mw.proofreadpage.PagelistInputWidget.Parameters}
  */
 VisualDialogModel.prototype.setNumbering = function ( params, data ) {
-	// If there is a number, and the ranges are the 3 builtin ranges, go ahead and set
-	// the numbering, else, delete whatever is already there ( if there are any numberings
-	// it will always be in the data... so we don't need to worry about preserving old ones.
-	if ( data.number && ( data.label === 'roman' || data.label === 'highroman' || data.label === 'Number' ) ) {
+	// If there is a number, and the range is in the list of builtin ranges and the range
+	// changes the number format,  go ahead and set the numbering, else, delete whatever
+	// is already there if there are any numberings it will always be in the data...
+	//  so we don't need to worry about preserving old ones.
+	if ( data.number && mw.config.get( 'prpPagelistBuiltinLabels' ) &&
+		mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( data.label ) !== -1 && data.label !== 'empty' ) {
 		params.set( this.data.subPage, data.number );
 	} else {
 		params.delete( this.data.subPage );
@@ -207,7 +209,8 @@ VisualDialogModel.prototype.removeUnecessaryNumbering = function ( params, parti
 		if ( index.split( 'to' )[ 0 ] === index ) {
 			if ( partialList[ index ] ) {
 				label = partialList[ index ][ 0 ].type;
-				if ( label !== 'roman' && label !== 'highroman' && label !== 'Number' ) {
+				if ( mw.config.get( 'prpPagelistBuiltinLabels' ) &&
+				mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( label ) === -1 ) {
 					params.delete( index );
 				}
 			}

includes/Index/EditIndexPage.php
@@ -12,6 +12,7 @@ use OOUI\MultilineTextInputWidget;
 use OOUI\TextInputWidget;
 use ProofreadPage\Context;
 use ProofreadPage\OOUI\PagelistInputWidget;
+use ProofreadPage\Pagination\PageNumber;
 use Title;
 use WebRequest;
 use WikitextContent;
@@ -97,6 +98,9 @@ class EditIndexPage extends EditPage {
 	private function buildField( CustomIndexField $field, $inputOptions ) {
 		$key = $this->getFieldNameForEntry( $field->getKey() );
 		$val = $field->getStringValue();
+		$out = $this->context->getOutput();
+
+		$out->addJsConfigVars( 'prpPagelistBuiltinLabels', PageNumber::getDisplayModes() );
 
 		$inputOptions['name'] = $key;
 		$inputOptions['value'] = $val;
@@ -119,7 +123,7 @@ class EditIndexPage extends EditPage {
 				'templateParameter' => $field->getKey(),
 				'rows' => $field->getSize()
 			], $inputOptions ) );
-			$this->context->getOutput()->addModules( 'ext.proofreadpage.index.pagelist' );
+			$out->addModules( 'ext.proofreadpage.index.pagelist' );
 		} else {
 			if ( $field->getSize() > 1 ) {
 				$input = new MultilineTextInputWidget( $inputOptions + [

commit 197f87abd18a97e712c6443bda2464de66310e99

Corrected check for builtin ranges in PagelistWidget
Fixed incorrect checks for builtin ranges introduced
in fc745f5dfb483766e12cf4123ef08cc1427d37a4 that rejected
the 'Number' input value.

Bug: T272187
Change-Id: Ia549b0f2d5e7bbf65e469da4176f9461906528d3

modules/index.pagelist/PagelistInputWidget.PageNumberInputWidget.js
@@ -115,8 +115,8 @@ PageNumberInputWidget.prototype.updateFormPanel = function () {
 
 	// Check if the label is one that is provided by ProofreadPage by default
 	// and changes the number format being displayed.
-	if ( mw.config.get( 'prpPagelistBuiltinLabels' ) &&
-	mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( data.label ) === -1 && data.label !== 'empty' ) {
+	if ( ( data === 'Number' || ( mw.config.get( 'prpPagelistBuiltinLabels' ) &&
+	mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( data ) !== -1 && data !== 'empty' ) ) ) {
 		if ( data === 'Number' ) {
 			this.emit( 'changedToSinglePageIncompatibleValue' );
 		} else {

modules/index.pagelist/PagelistInputWidget.VisualDialogModel.js
@@ -148,8 +148,8 @@ VisualDialogModel.prototype.setNumbering = function ( params, data ) {
 	// changes the number format,  go ahead and set the numbering, else, delete whatever
 	// is already there if there are any numberings it will always be in the data...
 	//  so we don't need to worry about preserving old ones.
-	if ( data.number && mw.config.get( 'prpPagelistBuiltinLabels' ) &&
-		mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( data.label ) !== -1 && data.label !== 'empty' ) {
+	if ( data.number && ( data.label === 'Number' || ( mw.config.get( 'prpPagelistBuiltinLabels' ) &&
+		mw.config.get( 'prpPagelistBuiltinLabels' ).indexOf( data.label ) !== -1 && data.label !== 'empty' ) ) ) {
 		params.set( this.data.subPage, data.number );
 	} else {
 		params.delete( this.data.subPage );