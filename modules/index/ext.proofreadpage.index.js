5b9354400699ec7e924281ee2dd7d3b6995365a7
DannyS712 <dannys712.wiki@gmail.com>
2021-09-20 23:08:21 +0000

    Remove unneeded mw.loader.using call for a dependency
    There is no need to verify that the
    mediawiki.widgets.CategoryMultiselectWidget module is
    loaded via mw.loader.using, since its listed as a dependency
    in extension.json it will always be loaded.
    
    Follow-up: Ieb16ad4a7a52d8ae2cf7794eea0ca47649a97042
    Follow-up: If5cc06a13348b5cf976dba1dfb6d4fc5ece5c132
    Change-Id: I89fa2688845c195d36175bd2f7b28cef3cdc8fad

--- modules/index/ext.proofreadpage.index.js
+++ modules/index/ext.proofreadpage.index.js
@@ -1,25 +1,23 @@
-mw.loader.using( 'mediawiki.widgets.CategoryMultiselectWidget' ).done( function () {
-	$( function () {
-		// eslint-disable-next-line no-jquery/no-global-selector
-		$( '.prp-fieldLayout-help' ).each( function () {
-			OO.ui.infuse( this );
+$( function () {
+	// eslint-disable-next-line no-jquery/no-global-selector
+	$( '.prp-fieldLayout-help' ).each( function () {
+		OO.ui.infuse( this );
+	} );
+
+	// Categories
+	// eslint-disable-next-line no-jquery/no-global-selector
+	$( '#wpPrpCategories' ).each( function () {
+		var categoryInputWidget = OO.ui.infuse( this.parentNode ),
+			categorySelector = new mw.widgets.CategoryMultiselectWidget( {} );
+
+		categorySelector.setValue( categoryInputWidget.getValue().split( '|' ).filter( function ( value ) {
+			return value !== '';
+		} ) );
+		categorySelector.on( 'change', function () {
+			categoryInputWidget.setValue( categorySelector.getValue().join( '|' ) );
 		} );
 
-		// Categories
-		// eslint-disable-next-line no-jquery/no-global-selector
-		$( '#wpPrpCategories' ).each( function () {
-			var categoryInputWidget = OO.ui.infuse( this.parentNode ),
-				categorySelector = new mw.widgets.CategoryMultiselectWidget( {} );
-
-			categorySelector.setValue( categoryInputWidget.getValue().split( '|' ).filter( function ( value ) {
-				return value !== '';
-			} ) );
-			categorySelector.on( 'change', function () {
-				categoryInputWidget.setValue( categorySelector.getValue().join( '|' ) );
-			} );
-
-			categoryInputWidget.$element.before( categorySelector.$element );
-			categoryInputWidget.toggle( false );
-		} );
+		categoryInputWidget.$element.before( categorySelector.$element );
+		categoryInputWidget.toggle( false );
 	} );
 } );
