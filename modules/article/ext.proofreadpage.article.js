821390246ac450592c5e5c8582645b16359038f7
Sohom <sohom.datta@learner.manipal.edu>
2022-05-30 16:51:43 +0530

    Replace proofreadpage_source_href with prpSourceIndexPage and build source link client side.
    Replace the proofreadpage_source_href config variable with the
    prpSourceIndexPage config var that will only contain the Index
    Page. Exposing this will allow scripts/other extensions to use
    the IndexPage without having to parse the source href link.
    
    Additionally modified ext.proofreadpage.article.js to build the
    source href client-side using prpSourceIndexPage.
    
    Bug: T309490
    Change-Id: I4a793551f5f9d654c7f8a586fae0dd854d7b3d3d

--- modules/article/ext.proofreadpage.article.js
+++ modules/article/ext.proofreadpage.article.js
@@ -1,10 +1,24 @@
 $( function () {
 	'use strict';
 	/* add backlink to index page */
+	var urlLink;
+	if ( mw.config.get( 'prpSourceIndexPage' ) ) {
+		urlLink = mw.html.element( 'a',
+			{
+				href: mw.util.getUrl( mw.config.get( 'prpSourceIndexPage' ) ),
+				title: mw.msg( 'proofreadpage_source_message' )
+			},
+			mw.msg( 'proofreadpage_source' ) );
+	} else {
+		// TODO(sohom): Remove 'proofreadpage_source_href' after a bit,
+		// currently supported so as to not break the functionality
+		// while trasfering to 'prpSourceIndexPage'
+		urlLink = mw.config.get( 'proofreadpage_source_href' );
+	}
 	// eslint-disable-next-line no-jquery/no-global-selector
 	$( '#ca-nstab-main' ).after(
 		$( '<li>' ).attr( 'id', 'ca-proofread-source' ).append(
-			$( '<span>' ).html( mw.config.get( 'proofreadpage_source_href' ) )
+			$( '<span>' ).html( urlLink )
 		)
 	);
 } );
