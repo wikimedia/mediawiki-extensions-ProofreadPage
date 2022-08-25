a9959131e93f84523885d99c55b3dcd9b8baba8b
Jan Drewniak <jan.drewniak@gmail.com>
2022-08-25 10:56:18 -0400

    Use mw.util.addPortletLink to attach "source" link
    Instead of manually inserting a link via jQuery, uses
    the `mw.util.addPortletLink()` method to insert the
    "Source" link in the toolbar.
    
    This prevents an issue in Vector 2022 where the element
    is expected to have a 'mw-list-item` class.
    
    Bug: T316169
    Change-Id: I76eb666be920b82a907f3f39fb2fb693645309b8

--- modules/article/ext.proofreadpage.article.js
+++ modules/article/ext.proofreadpage.article.js
@@ -1,24 +1,34 @@
 $( function () {
 	'use strict';
 	/* add backlink to index page */
-	var urlLink;
+	var linkUrl;
+
 	if ( mw.config.get( 'prpSourceIndexPage' ) ) {
-		urlLink = mw.html.element( 'a',
-			{
-				href: mw.util.getUrl( mw.config.get( 'prpSourceIndexPage' ) ),
-				title: mw.msg( 'proofreadpage_source_message' )
-			},
-			mw.msg( 'proofreadpage_source' ) );
+		linkUrl = mw.util.getUrl( mw.config.get( 'prpSourceIndexPage' ) );
 	} else {
 		// TODO(sohom): Remove 'proofreadpage_source_href' after a bit,
 		// currently supported so as to not break the functionality
 		// while trasfering to 'prpSourceIndexPage'
-		urlLink = mw.config.get( 'proofreadpage_source_href' );
+		var anchorEl = $.parseHTML(
+			mw.config.get( 'proofreadpage_source_href' )
+		);
+		linkUrl = anchorEl.attr( 'href' );
 	}
-	// eslint-disable-next-line no-jquery/no-global-selector
-	$( '#ca-nstab-main' ).after(
-		$( '<li>' ).attr( 'id', 'ca-proofread-source' ).append(
-			$( '<span>' ).html( urlLink )
-		)
+
+	mw.util.addPortletLink(
+		// Namespaces menu
+		'p-namespaces',
+		// link URL
+		linkUrl,
+		// visual label message
+		mw.msg( 'proofreadpage_source' ),
+		// element ID
+		'ca-proofread-source',
+		// tooltip (title attribute) message
+		mw.msg( 'proofreadpage_source_message' ),
+		// accesskey
+		null,
+		// insert after talk page link
+		'#ca-talk'
 	);
 } );
