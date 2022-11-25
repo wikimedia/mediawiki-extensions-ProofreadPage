a2a9878f56c8770f68942ff7c5f2410bf7b890d2
Sohom <sohomdatta1+git@gmail.com>
2022-11-25 16:50:43 +0530

    Remove defunct grunt build commands
    Follow-Up: If3a92a43da0da398339892b54472d31d70700134
    Change-Id: I08195b3f760ace725c35a8abfc1157a16acf7007

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
-	grunt.loadNpmTasks( 'grunt-replace' );
 
 	grunt.initConfig( {
 		eslint: {
@@ -34,5 +33,4 @@ module.exports = function ( grunt ) {
 		grunt.task.run( [ 'eslint', 'stylelint' ] );
 	} );
 	grunt.registerTask( 'default', 'test' );
-	grunt.registerTask( 'build', 'replace' );
 };
