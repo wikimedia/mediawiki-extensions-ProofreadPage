124e6ca43e47d303ed04ae410ddc736311331636
Sohom <sohomdatta1+git@gmail.com>
2023-01-29 20:53:27 +0530

    Add internal API to retrieve default content for a Page: page
    This API will allow EditInSequence to dynamically load the
    default content for a particular Page: page without loading
    the whole edit page.
    
    Marking as internal due to the fact that we include potentially
    untrusted content from book textlayers.
    
    Bug: T308098
    Change-Id: Iace82c645b7b047d505d9b288a0768cf2c0d2e6b

---
+++ includes/Api/ApiQueryDefaultContentForPage.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ProofreadPage\Api;
+
+use ApiQueryBase;
+use MediaWiki\MediaWikiServices;
+use ProofreadPage\Context;
+use ProofreadPage\Page\PageContentBuilder;
+use ProofreadPage\Page\PageContentHandler;
+use Title;
+
+class ApiQueryDefaultContentForPage extends ApiQueryBase {
+	/**
+	 * @var Context
+	 */
+	private $context;
+
+	/**
+	 * @var PageContentBuilder
+	 */
+	private $pageContentBuilder;
+
+	/**
+	 * @var PageContentHandler
+	 */
+	private $pageContentHandler;
+
+	/** @var string API module prefix */
+	private static $prefix = 'prppdefaultcontent';
+
+	/**
+	 * @param \ApiQuery $query
+	 * @param string $moduleName
+	 */
+	public function __construct( $query, $moduleName ) {
+		parent::__construct( $query, $moduleName, static::$prefix );
+		$this->context = $this->context = Context::getDefaultContext();
+		$this->pageContentBuilder = new PageContentBuilder( $this, $this->context );
+		$this->pageContentHandler = MediaWikiServices::getInstance()
+			->getContentHandlerFactory()
+			->getContentHandler( CONTENT_MODEL_PROOFREAD_PAGE );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute() {
+		$pageSet = $this->getPageSet()->getGoodAndMissingPages();
+		$result = $this->getResult();
+
+		foreach ( $pageSet as $pageID => $page ) {
+
+			if ( $page->getNamespace() !== $this->context->getPageNamespaceId() ) {
+				continue;
+			}
+
+			$title = Title::castFromPageIdentity( $page );
+
+			if ( !$title ) {
+				continue;
+			}
+
+			$result->addValue( [ 'query', 'pages', $pageID ], 'defaultcontentforpage',
+				$this->pageContentHandler->serializeContent(
+					$this->pageContentBuilder->buildDefaultContentForPageTitle( $title ),
+					CONTENT_FORMAT_WIKITEXT
+				)
+			);
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getAllowedParams() {
+		return [];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function isInternal() {
+		return true;
+	}
+}
