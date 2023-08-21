8d23aa8dbac7b1593c24de115c39ff3eb8b3c41b
Zabe <zabe@avorwerk.net>
2023-08-21 11:42:16 +0000

    Revert "Revert "add su namespace translations""
    This reverts commit 90ca0918f3262a7f0db3be506d1ac59bfb8119ff.
    
    Change-Id: I282c01b3641c385877977757f1c454a76716248f

--- ProofreadPage.namespaces.php
+++ ProofreadPage.namespaces.php
@@ -449,6 +449,14 @@ $proofreadPageNamespaceNames['sr-el'] = [
 	'index_talk' => 'Razgovor_o_indeksu'
 ];
 
+/** Sundanese (basa Sunda) */
+$proofreadPageNamespaceNames['su'] = [
+	'page' => 'Kaca',
+	'page_talk' => 'Obrolan_kaca',
+	'index' => 'Indéks',
+	'index_talk' => 'Obrolan_indéks'
+];
+
 /** Swedish (svenska) */
 $proofreadPageNamespaceNames['sv'] = [
 	'page' => 'Sida',
