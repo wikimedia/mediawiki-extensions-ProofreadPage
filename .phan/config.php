a4458a4a1a97b325ef1224cdeecbd597a561dbef
Sohom <sohomdatta1+git@gmail.com>
2023-08-23 19:53:48 +0530

    Move edit-in-sequence behind a beta feature
    Based on the feedback currently given there are some issues
    with our current implementation. We should gate this behind
    a user-enabled beta feature untill we have fixed some of the
    more egregious issues (specifically the one which leaves
    unsaved text behind).
    
    Bug: T308098
    Change-Id: Ie108817bc7dd6c76a73f6229f1cc0c559cd9a86d

--- .phan/config.php
+++ .phan/config.php
@@ -12,6 +12,7 @@ $cfg['file_list'] = array_merge(
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
+		'../../extensions/BetaFeatures',
 		'../../extensions/Scribunto',
 	]
 );
@@ -19,6 +20,7 @@ $cfg['directory_list'] = array_merge(
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
+		'../../extensions/BetaFeatures',
 		'../../extensions/Scribunto',
 	]
 );
