c19b9a7f9894de9f808667251c8a2dca8ef0a3d2
Željko Filipin <zeljko.filipin@gmail.com>
2026-03-24 09:16:38 +0100

    selenium: reload php-fpm instead of restart
    In the CI environment, PHP Fpm is never revalidating files once they
    have entered the opcache (`opcache.validate_timestamps=0`).
    
    The first request hitting MediaWiki triggers caching of
    `LocalSettings.php` and subsequent changes to it (via
    `overrideLocalSettings()` will thus not been taken in account since PHP
    serves it from the stalled cache (as intended).
    
    The issue notably happens when running tests in parallel, some test
    suites might not alter the `LocalSettings.php`, the stock one is thus
    cached and when another tests changes the file, the new settings are now
    taken in account on any test relying on the change ends up failing.
    
    We hit that case for the API Testing suite and Selenium:
    https://phabricator.wikimedia.org/T276428#7194025 .
    
    The solution introduced to clear the opcache was to restart the php-fpm.
    Since the service is managed by Supervisor:
    - on the first restart, it did not exit properly leaving a zombie and
      not starting a new instance.
    - the second invocation hangs for 30 seconds trying to kill the zombie
      before giving up and eventually starting a new process.
    
    I have confirmed reloading php-fpm is sufficient to clear the opcache.
    The init script look up a php-fpm process and issues it a SIGHUP2 which
    reloads the worker and clear the opcache:
    https://phabricator.wikimedia.org/T418369#11703410
    
    - Change the service command from `restart` to `reload`. Drop the now
    useless second `restart`.
    
    Outcome:
    - the CI jobs will be slightly faster they will no more hang for 30
      seconds for the php-fpm zombie process (in some case multiple times)
    - the suite no more interfers with the php-fpm process managed by
      Supervisord which might have different settings than the stock default
      one which is invoked when restarting. Notably stdout/stderr.
    
    Bug: T420584
    Change-Id: I287718c92a7b33025882410d674dfbb90bf25b52

--- tests/selenium/wdio.conf.js
+++ tests/selenium/wdio.conf.js
@@ -28,17 +28,33 @@ export const config = {
 		}
 		` );
 
-		// per T315214#8153130 and GrowthExperiments implementation of the same
-		// at https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/GrowthExperiments/+/refs/heads/master/tests/selenium/wdio.conf.js#5
+		/**
+		 * Reset the PHP-Fpm opcache under Quibble environment
+		 *
+		 * In the CI environment, PHP Fpm is never revalidating files once they have
+		 * entered the opcache (`opcache.validate_timestamps=0`).
+		 *
+		 * The first request hitting MediaWiki triggers caching of `LocalSettings.php`
+		 * and subsequent changes to it (via `overrideLocalSettings()` will thus not
+		 * been taken in account since PHP serves it from the stalled cache (as
+		 * intended).
+		 *
+		 * The issue notably happens when running tests in parallel, some test suites
+		 * might not alter the `LocalSettings.php`, the stock one is thus cached and
+		 * when another tests changes the file, the new settings are now taken in
+		 * account on any test relying on the change ends up failing.
+		 *
+		 * We hit that case for the API Testing suite and Selenium:
+		 * https://phabricator.wikimedia.org/T276428#7194025
+		 *
+		 * The PHP-Fpm opcache is held in shared memory and we thus can not clear it
+		 * using `opcache_reset()` from the PHP CLI. However the opcache is cleared
+		 * when reloading PHP-Fpm https://phabricator.wikimedia.org/T418369#11703410
+		 */
 		if ( process.env.QUIBBLE_APACHE ) {
 			spawnSync(
 				'service',
-				[ phpFpmServiceName, 'restart' ]
-			);
-			// Super ugly hack: Run this twice because sometimes the first invocation hangs.
-			spawnSync(
-				'service',
-				[ phpFpmServiceName, 'restart' ]
+				[ phpFpmServiceName, 'reload' ]
 			);
 		}
 	},
